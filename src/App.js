5dff28ebda87d6b99cbba59f979ba7126c6246b8
brianhitchin <brianleehitchin@gmail.com>
2023-06-23 08:51:47 -0700

    starting reserv

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [availtimes, setAvailtimes] = useState([])
+
+  function initializeTimes() {
+    const today = new Date();
+
+    fetchAPI(today)
+      .then((available_times) => {
+        updateTimes(available_times);
+      })
+      .catch((_error) => {
+        console.error('Error fetching available times at this particular date.');
+      });
+  }
+
+  function updateTimes(selectedDate) {
+    fetchAPI(selectedDate)
+      .then((times) => {
+        // Update the times in the booking form
+        // ...
+      })
+      .catch((error) => {
+        console.error('Error fetching available times:', error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
