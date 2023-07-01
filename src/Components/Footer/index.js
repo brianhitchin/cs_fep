844cf9fc48d554a75e03b9de0f808e6452dc2684
brianhitchin <brianleehitchin@gmail.com>
2023-07-01 14:53:46 -0700

    hp done

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css'
 
-export default function Footer() {
+export default function FooterFunc() {
     return (
         <footer className="fmain">
             Copyright 2023 Little Lemon ©. All rights reserved.
--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,6 +1,9 @@
 import './index.css'
 import Header from '../Header';
 import Navbar from '../Nav';
+import FooterFunc from '../Footer';
+import lasag from './lasag.jpg'
+import spag from './spag.jpg'
 
 const HomePage = () => {
     return (
@@ -13,6 +16,20 @@ const HomePage = () => {
                     to Chicago. Drop by!
                 </p>
             </section>
+            <section className='specials'>
+                <h4>Daily Specials</h4>
+                <div className='spdv'>
+                    <article className='spar'>
+                        <img src={spag} alt="spag"></img>
+                        <div>Spaghetti, $8.99</div>
+                    </article>
+                    <article className='spar'>
+                        <img src={lasag} alt="spag"></img>
+                        <div>Lasagne, $9.99</div>
+                    </article>
+                </div>
+            </section>
+            <FooterFunc />
         </main>
     )
 }
